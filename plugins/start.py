6bb832c9ee3ace0823d8070eafba719ae7584ab6
Hare Krishna <128279576+tushardhab@users.noreply.github.com>
2024-12-31 22:12:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -54,7 +54,7 @@ async def start_command(client: Client, message: Message):
         try:
             messages = await get_messages(client, ids)
         except:
-            await message.reply_text("Something went wrong..!")
+            await message.reply_text("KUCH TO GADBAD HAI..!")
             return
         await temp_msg.delete()
 
@@ -156,7 +156,7 @@ async def start_command(client: Client, message: Message):
 
 WAIT_MSG = """"<b>Processing ...</b>"""
 
-REPLY_ERROR = """<code>Use this command as a replay to any telegram message with out any spaces.</code>"""
+REPLY_ERROR = """<code>DONT TYPE ANYTHING.</code>"""
 
 #=====================================================================================##
 
